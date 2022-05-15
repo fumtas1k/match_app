10032a1fb5d8d0fa1c7434765a005c9a1b3ceb6b
fumtas1k <fumta01a7@gmail.com>
2022-05-15 17:56:51 +0900

    fix:構文修正202205151756

--- app/javascript/src/profile_image_upload.js
+++ app/javascript/src/profile_image_upload.js
@@ -1,6 +1,6 @@
 $(document).on("change", "#user_profile_image", function(e){
   if (e.target.files.length) {
-    let reader = new FileReader;
+    const reader = new FileReader;
     reader.onload = function(e) {
       $(".profile-default-img").removeClass();
       $("#profile-img").remove();
