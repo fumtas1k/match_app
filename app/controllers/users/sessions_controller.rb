679782656367272460defb973699bfab56e90c01
fumtas1k <fumta01a7@gmail.com>
2022-05-18 18:12:29 +0900

    add:ゲストログイン機能追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,14 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    if params[:guest]
+      sign_in(User.guest)
+      redirect_to users_path
+    else
+      super
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
